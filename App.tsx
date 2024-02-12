c60a3bb53642f179df6e9da52cb2ae4254b62dcd
Gabriel <gabriel.merio.dev@gmail.com>
2024-02-11 23:31:06 -0300

    pushing up

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { AppProvider, UserProvider } from '@realm/react';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
+import { REALM_APP_ID } from '@env';
+
+import { Routes } from './src/routes';
+
 import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
-import { StatusBar } from 'react-native';
-import {AppProvider, UserProvider} from '@realm/react'
-import { REALM_APP_ID } from '@env';
-import { Home } from './src/screens/Home';
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold
@@ -24,18 +27,17 @@ export default function App() {
 
   return (
     <AppProvider id={REALM_APP_ID}>
-      <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
         <StatusBar 
           barStyle="light-content" 
           backgroundColor="transparent" 
           translucent 
         />
         <UserProvider fallback={SignIn}>
-          <Home />
-        </UserProvider> 
+          <Routes />
+        </UserProvider>
       </ThemeProvider>
     </AppProvider>
+    
   );
 }
-
-
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,29 +2,37 @@ import { TouchableOpacity } from 'react-native'
 import { Power } from 'phosphor-react-native'
 
 import { Container, Greeting, Message, Name, Picture } from './styles';
+import { useApp, useUser } from '@realm/react';
 
 import theme from '../../theme';
 
 
 export function Header() {
+  const user = useUser();
+  const app = useApp();
+
+  function handleLogout() {
+    app.currentUser?.logOut();
+  }
+
   return (
     <Container>
       <Picture 
-        source={{ uri: 'https://github.com/rennand.png' }}
+        source={{ uri: user.profile.pictureUrl }}
         placeholder='L184i9ofbHof00ayjsay~qj[ayj@'
       />
       <Greeting>
 
         <Message>
-          Olá
+          Olá,
         </Message>
 
         <Name>
-          Rodrigo
+          {user?.profile.name}
         </Name>
       </Greeting>
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={handleLogout} activeOpacity={0.7}>
         <Power size={32} color={theme.COLORS.GRAY_400} />
       </TouchableOpacity>
     </Container>
