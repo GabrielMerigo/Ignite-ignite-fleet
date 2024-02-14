390e0936d27e8511d51fc59c3cd5acdbd3a9b092
Gabriel <gabriel.merio.dev@gmail.com>
2024-02-13 22:02:55 -0300

    creating data into realm

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,11 +1,95 @@
+import { useRef, useState } from 'react';
+import { useUser } from '@realm/react';
+import { TextInput, ScrollView, KeyboardAvoidingView, Platform, Alert } from 'react-native';
+import { useRealm } from '../../libs/realm';
+
+import { Button } from '../../components/Button';
+import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { TextAreaInput } from '../../components/TextAreaInput';
+
+import { Container, Content } from './styles';
+import { licensePlateValidate } from '../../utils/licensePlateValidate';
 import { MainHeader } from '../../components/MainHeader';
-import { Container } from './styles';
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { useNavigation } from '@react-navigation/native';
+
+const keyboardAvoidingViewBehavior = Platform.OS === 'android' ? 'height' : 'position';
 
 export function Departure() {
+  const { goBack } = useNavigation();
+  const user = useUser();
+  const [description, setDescription] = useState('');
+  const [licensePlate, setLicensePlate] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const realm = useRealm();
+
+  const descriptionRef = useRef<TextInput>(null);
+
+  function handleDepartureRegister() {
+    try {
+      if(!licensePlateValidate(licensePlate)) {
+        return Alert.alert('Placa inválida', 'A placa é inválida. Por favor, informa a placa correta.')
+      }
+  
+      if(description.trim().length === 0) {
+        descriptionRef.current?.focus();
+        return Alert.alert('Finalidade', 'Por favor, informe a finalidade da utilização do veículo')
+      }
+
+      setIsRegistering(true)
+
+      realm.write(() => {
+        realm.create('Historic', Historic.generate({
+          user_id: user!.id,
+          license_plate: licensePlate.toUpperCase(),
+          description,
+        }))
+      });
+
+      Alert.alert('Saída', 'Saída do veículo registrada com sucesso.')
+      goBack();
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Erro', 'Não foi possível registrar a saída do veículo')
+      setIsRegistering(false)
+    }
+
+  }
+
   return (
     <Container>
-
       <MainHeader title='Saída' />
+
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior={keyboardAvoidingViewBehavior} >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label='Placa do veículo'
+              placeholder="BRA1234"
+              returnKeyType='next'
+              onChangeText={setLicensePlate}
+            />
+
+            <TextAreaInput
+              ref={descriptionRef}
+              label='Finalizade'
+              placeholder='Vou utilizar o veículo para...'
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType='send'
+              blurOnSubmit
+              onChangeText={setDescription}
+            />
+
+            <Button 
+              title='Registar Saída'
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,21 +4,32 @@ import { Container, Content } from './styles';
 
 import { Header } from '../../components/Header';
 import { CarStatus } from '../../components/CarStatus';
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { useEffect } from 'react';
+import { useQuery } from '../../libs/realm';
 
 export function Home() {
-
+  const historic = useQuery(Historic);
   const { navigate } = useNavigation();
 
-  function handleRegisterMoviment() {
+  function handleRegisterMovement() {
     navigate('departure')
   }
+
+  function fetchVehicle() {
+    console.log(historic)
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, [])
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMoviment} />
+        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMovement} />
       </Content>
     </Container>
   );
