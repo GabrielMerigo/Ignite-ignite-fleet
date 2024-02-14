7f06a175b908c494224c8c3122de1bc09de4435e
Gabriel <gabriel.merio.dev@gmail.com>
2024-02-13 22:43:49 -0300

    more infos

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,22 +1,79 @@
-import { useRoute } from '@react-navigation/native';
+import { Alert } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { X } from 'phosphor-react-native';
 
-import { Container } from './styles';
+import { useObject, useRealm } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
+import { BSON } from 'realm';
+
+import { Container, Content, Description, Footer, Label, LicensePlate } from './styles';
+
+import { Button } from '../../components/Button';
+import { ButtonIcon } from '../../components/ButtonIcon';
+import { MainHeader } from '../../components/MainHeader';
 
 type RouteParamProps = {
   id: string;
 }
 
 export function Arrival() {
-  const route = useRoute();
 
+  const route = useRoute();
   const { id } = route.params as RouteParamProps;
 
 
-  console.log(id);
+  const realm = useRealm();
+  const { goBack } = useNavigation();
+  const historic = useObject(Historic, new BSON.UUID(id) as unknown as string);
 
+  function handleRemoveVehicleUsage() {
+    Alert.alert(
+      'Cancelar',
+      'Cancelar a utilização do veículo?',
+      [
+        { text: 'Não', style: 'cancel' },
+        { text: 'Sim', onPress: () => removeVehicleUsage() },
+      ]
+    )
+  }
+
+  function removeVehicleUsage() {
+    realm.write(() =>{
+      realm.delete(historic)
+    });
+
+    goBack();
+  }
+  
   return (
     <Container>
+      <MainHeader title='Chegada' />
+      <Content>
+        <Label>
+          Placa do veículo
+        </Label>
 
+        <LicensePlate>
+          {historic?.license_plate}
+        </LicensePlate>
+
+        <Label>
+          Finalidade
+        </Label>
+
+        <Description>
+          {historic?.description}
+        </Description>
+
+        <Footer>
+          <ButtonIcon 
+            icon={X} 
+            onPress={handleRemoveVehicleUsage}
+          />
+
+          <Button title='Registrar chegada' />
+        </Footer>
+      </Content>
     </Container>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { useQuery } from '../../libs/realm';
+import { useQuery, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
 
 import { Header } from '../../components/Header';
@@ -11,13 +11,14 @@ import { CarStatus } from '../../components/CarStatus';
 import { Container, Content } from './styles';
 
 export function Home() {
+  const realm = useRealm();
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
 
   const { navigate } = useNavigation();
 
   const historic = useQuery(Historic)
 
-  function handleRegisterMoviment() {
+  function handleRegisterMovement() {
     if(vehicleInUse?._id) {
       navigate('arrival', { id: vehicleInUse._id.toString() });
     } else {
@@ -25,7 +26,7 @@ export function Home() {
     }
   }
 
-  function fetchVehicle() {
+  function fetchVehicleInUse() {
     try {
       const vehicle = historic.filtered("status='departure'")[0];
       setVehicleInUse(vehicle);
@@ -35,8 +36,18 @@ export function Home() {
     }
   }
 
+  // Ver como usar apenas uma vez essa função
   useEffect(() => {
-    fetchVehicle();
+    fetchVehicleInUse();
+  }, [])
+
+  // Observar mudanças no banco do realm
+  useEffect(() => {
+    realm.addListener('change', () => {
+      fetchVehicleInUse();
+    })
+
+    return () => { realm.removeListener('change', fetchVehicleInUse) }
   },[])
 
   return (
@@ -46,7 +57,7 @@ export function Home() {
       <Content>
         <CarStatus 
           licensePlate={vehicleInUse?.license_plate}
-          onPress={handleRegisterMoviment} 
+          onPress={handleRegisterMovement} 
         />
       </Content>
     </Container>
