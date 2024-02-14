f019148b2fae4cc797b2479533c6224e8501c754
Gabriel <gabriel.merio.dev@gmail.com>
2024-02-13 22:13:06 -0300

    added new arrival screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,35 +1,53 @@
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
-import { Container, Content } from './styles';
+import { useQuery } from '../../libs/realm';
+import { Historic } from '../../libs/realm/schemas/Historic';
 
 import { Header } from '../../components/Header';
 import { CarStatus } from '../../components/CarStatus';
-import { Historic } from '../../libs/realm/schemas/Historic';
-import { useEffect } from 'react';
-import { useQuery } from '../../libs/realm';
+
+import { Container, Content } from './styles';
 
 export function Home() {
-  const historic = useQuery(Historic);
+  const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null);
+
   const { navigate } = useNavigation();
 
-  function handleRegisterMovement() {
-    navigate('departure')
+  const historic = useQuery(Historic)
+
+  function handleRegisterMoviment() {
+    if(vehicleInUse?._id) {
+      navigate('arrival', { id: vehicleInUse._id.toString() });
+    } else {
+      navigate('departure')
+    }
   }
 
   function fetchVehicle() {
-    console.log(historic)
+    try {
+      const vehicle = historic.filtered("status='departure'")[0];
+      setVehicleInUse(vehicle);
+    } catch (error) {
+      Alert.alert('Veículo em uso', 'Não foi possível carregar o veículo em uso.');
+      console.log(error);
+    }
   }
 
   useEffect(() => {
     fetchVehicle();
-  }, [])
+  },[])
 
   return (
     <Container>
       <Header />
 
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMovement} />
+        <CarStatus 
+          licensePlate={vehicleInUse?.license_plate}
+          onPress={handleRegisterMoviment} 
+        />
       </Content>
     </Container>
   );
